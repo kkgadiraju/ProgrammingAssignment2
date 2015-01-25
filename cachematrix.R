7d429452cde077b8da82569f509d9bd5b51b01c5
Krishna Karthik Gadiraju <kkgadiraju@gmail.com>
2015-01-25 08:44:03 -0500

    Added a few more comments and minor modifications to naming terminology

--- cachematrix.R
+++ cachematrix.R
@@ -3,23 +3,24 @@
 ##Once the value has been stored, every other time it is called, data is pulled out of cache
 
 ## Creates a cache matrix and stores it in a variable named matrix
-##returns a list of functions which can be called from another function
+##returns a list of functions which can be called from another function and can be used to set the value of the matrix,
+## set the value of its inverse, or get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-          matrix<-NULL
+          matrixInverse<-NULL
           set<-function(y){
             x<<-y
-            matrix<<-NULL
+            matrixInverse<<-NULL
           }
           get<-function() x
-          setInverse<-function(inv)matrix<<-inv
-          getInverse<-function() matrix
+          setInverse<-function(inv)matrixInverse<<-inv
+          getInverse<-function() matrixInverse
           list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Check if matrix is in cache, if value returned is not null, print it. Otherwise, calcualate its inverse and store it
-
+## If matrix has been altered, it is done by x$set() function, which will automatically set its corresponding inverse to null
 cacheSolve <- function(x, ...) {
       
   matrix<-x$getInverse()
