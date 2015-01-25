c0ba47e2645f9b783c3c0f12a2275632bddc9ba6
Krishna Karthik Gadiraju <kkgadiraju@gmail.com>
2015-01-25 08:06:24 -0500

    Modified functions, code structure similar to example supplied to programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          matrix<-NULL
+          set<-function(y){
+            x<<-y
+            matrix<<-NULL
+          }
+          get<-function() x
+          setInverse<-function(inv)matrix<<-inv
+          getInverse<-function() matrix
+          list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matrix<-x$getInverse()
+  if(!is.null(matrix)){
+    print("Getting cached data")
+    return(matrix)
+  }
+  myMatrix<-x$get()
+  matrix<-solve(myMatrix,...)
+  x$setInverse(matrix)
+  matrix
 }
